b34fcba1d9441678e52d43749a623ab40693b39e
alexey salimzyanov <salij3@mail.ru>
2024-04-11 18:11:21 +0700

    поправка

--- server/controllers/auditoriumController.js
+++ server/controllers/auditoriumController.js
@@ -3,13 +3,10 @@ const ApiError = require('../error/ApiError')
 
 class AuditoriumController{
     async create(req,res){
-        const {name, capacity, type} = req.body
-        const Auditorium = await AuditoriumList.create({name, capacity, type})// ne robit
-        return res.json(Auditorium)
+
     }
     async getAll(req,res){
-        const Auditoriums = await AuditoriumList.findAll()
-        return res.json(Auditoriums)
+
     }
 
 }
