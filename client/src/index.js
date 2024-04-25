2e8938e12ff4bd81b8de7d241027e805e13a92de
alexey salimzyanov <salij3@mail.ru>
2024-04-25 17:10:51 +0700

    Revert "главное меню + небольшие правки"
    This reverts commit 245dd29b59f70475469d44a546b41f4099f47780.

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,13 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserStore from './store/UserStore';
-import ScheduleViewStore from './store/ScheduleViewStore';
-import DayOfWeekStore from './store/DayOfWeekStore';
 
 export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Context.Provider value={{
-    user: new UserStore(),
-    view: new ScheduleViewStore(),
-    day: new DayOfWeekStore()
+    user: new UserStore()
   }}>
   <React.StrictMode>
     <App />
